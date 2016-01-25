1bef9d455e138e5e9ab9e2e4c4c3240a9be39388
alexishinse <alexishinse@hotmail.com>
2016-01-25 11:12:34 -0500

    ListView

--- app/src/main/java/csf/ca/typhonplayer/MainActivity.java
+++ app/src/main/java/csf/ca/typhonplayer/MainActivity.java
@@ -1,13 +1,24 @@
 package csf.ca.typhonplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.player);
+    }
+
+    public void displaySongList(View view)
+    {
+        Intent i = new Intent(getApplicationContext(), SongDisplay.class);
+
+        startActivity(i);
+
     }
 }
