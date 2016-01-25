1e1c3417a0ab2d51b042fd0ba5868abf25d832d1
alexishinse <alexishinse@hotmail.com>
2016-01-25 11:31:25 -0500

    songManager debut

---
+++ app/src/main/java/csf/ca/typhonplayer/SongsManager.java
@@ -0,0 +1,14 @@
+package csf.ca.typhonplayer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Alexis on 2016-01-25.
+ */
+public class SongsManager {
+    final String AUDIO_PATH = new String("/sdcard/");
+    private ArrayList<HashMap<String,String>> songsList = new ArrayList<HashMap<String,String>>();
+
+
+}
